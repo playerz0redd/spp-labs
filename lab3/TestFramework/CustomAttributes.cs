4312b333c292e3359b2fe0e9e1b29184c69af098
playerz0redd <154872216+playerz0redd@users.noreply.github.com>
2026-03-22 22:43:46 +0300

    upload lab3

---
+++ lab3/TestFramework/CustomAttributes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TestFramework
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TestAttribute : Attribute
+    {
+        public int Priority { get; set; } = 0;
+        public string Description { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class TestCaseAttribute : Attribute
+    {
+        public object[] Parameters { get; }
+        public TestCaseAttribute(params object[] parameters) => Parameters = parameters;
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class IgnoreAttribute : Attribute
+    {
+        public string Reason { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TimeoutAttribute : Attribute
+    {
+        public int Milliseconds { get; }
+        public TimeoutAttribute(int milliseconds) => Milliseconds = milliseconds;
+    }
+
+    [AttributeUsage(AttributeTargets.Method)] public class BeforeAttribute : Attribute { }
+    [AttributeUsage(AttributeTargets.Method)] public class AfterAttribute : Attribute { }
+}
